66473ee2565f976244329b08c63f94e4482facfe
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-06-22 23:48:21 +0300

    test and deploy to repo.codeborne.com

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ MODULE="geoip"
 VERSION=`grep version= build.gradle | sed "s/version='//g" | sed "s/'//g"`
 TARGET=/var/www/repo/$ORGANIZATION/$MODULE/$MODULE-$VERSION.jar
 
-./gradle clean install
+./gradle clean test install
 
 if [[ "$VERSION" == *SNAPSHOT ]]
 then
