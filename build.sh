3b82276a7264a0f3945ef5919366a0da9c34cb78
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-06-25 23:51:51 +0300

    publish geoip.jar to both repo.codeborne.com and maven repo

--- build.sh
+++ build.sh
@@ -7,15 +7,28 @@ TARGET=$REPO/$MODULE-$VERSION.jar
 
 ./gradle clean test install
 
+echo ""
+echo ""
+echo ""
+
 if [[ "$VERSION" == *SNAPSHOT ]]
 then
-  echo "Publish $TARGET (overwrite snapshot)"
-  cp build/libs/$MODULE-$VERSION.jar $TARGET
+  echo "Skip publishing $TARGET (nobody needs snapshot)"
 elif [ -e $TARGET ]; then
   echo "Not publishing ($MODULE-$VERSION already exists)"
 elif [ -e $REPO ]; then
-  echo "Publish $TARGET"
+
+  echo ""
+  echo ""
+  echo ""
+  echo " ********************************************************* "
   cp build/libs/$MODULE-$VERSION.jar $TARGET
+  echo "Published $TARGET"
+  echo " ********************************************************* "
+  echo ""
+  echo ""
+  echo ""
+
   ./gradle -b deploy.gradle uploadArchives --info --stacktrace
 else
   echo "Not publishing ($REPO does not exists)"
