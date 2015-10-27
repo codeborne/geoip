237da323bff955a11bb348c6629e63f6f812637e
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-10-27 21:15:10 +0200

    geoip 1.2: upgrade database file to latest state available from http://dev.maxmind.com/geoip/legacy/geolite/

--- test/com/codeborne/geoip/LookupServiceTest.java
+++ test/com/codeborne/geoip/LookupServiceTest.java
@@ -11,10 +11,13 @@ import static org.junit.Assert.*;
 public class LookupServiceTest {
   LookupService lookupService;
 
-  @Test
+  @Before
   public void usesProvidedDatabaseFile() throws IOException {
     lookupService = LookupService.indexCache();
+  }
 
+  @Test
+  public void location1() throws IOException {
     Location location = lookupService.getLocation("217.71.44.212");
     assertEquals("EE", location.countryCode);
     assertEquals("Estonia", location.countryName);
@@ -22,12 +25,25 @@ public class LookupServiceTest {
     assertEquals("01", location.region);
     assertEquals(59.4339, location.latitude, 0.0001);
     assertEquals(24.728104, location.longitude, 0.000001);
+  }
+  
+  @Test
+  public void location2() throws IOException {
+    Location location = lookupService.getLocation("213.172.3.234");
+    assertEquals("RU", location.countryCode);
+    assertEquals("Russian Federation", location.countryName);
+    assertEquals(55.75, location.latitude, 0.01);
+    assertEquals(37.616592, location.longitude, 0.000001);
+  }
 
-    Location location2 = lookupService.getLocation("213.172.3.234");
-    assertEquals("RU", location2.countryCode);
-    assertEquals("Russian Federation", location2.countryName);
-    assertEquals(55.75, location2.latitude, 0.01);
-    assertEquals(37.616592, location2.longitude, 0.000001);
+  @Test
+  public void location3() throws IOException {
+    Location location = lookupService.getLocation("85.117.103.134");
+    assertEquals("KZ", location.countryCode);
+    assertEquals("Kazakhstan", location.countryName);
+    assertEquals("Almaty", location.city);
+    assertEquals(43.2565, location.latitude, 0.01);
+    assertEquals(76.9284, location.longitude, 0.0001);
   }
 
   @After
